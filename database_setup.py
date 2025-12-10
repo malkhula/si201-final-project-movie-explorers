91c7bf27ccddc2240e0b0780ec97313d5c792220
malkhula <malkhula@umich.edu>
2025-12-09 21:52:59 -0500

    Finished code

--- database_setup.py
+++ database_setup.py
@@ -1,1 +1,43 @@
+import sqlite3
+
+#db setup
+conn = sqlite3.connect("movies.db")
+cur = conn.cursor()
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS tmdb_movies (
+ id INTEGER PRIMARY KEY,
+ title TEXT,
+ popularity REAL,
+ vote_average REAL,
+ genre_ids TEXT
+);
+""")
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS tmdb_genres (
+ genre_id INTEGER PRIMARY KEY,
+ genre_name TEXT
+);
+""")
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS omdb_movies (
+ title TEXT PRIMARY KEY,
+ imdb_rating REAL,
+ box_office REAL,
+ runtime TEXT
+);
+""")
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS tastedive_recommendations (
+ movie_title TEXT,
+ related_movie TEXT
+);
+""")
+
+conn.commit()
+conn.close()
+print("Database and tables created successfully!")
 
